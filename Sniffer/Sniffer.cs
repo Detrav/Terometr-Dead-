915bf3333bc64ef8beba040ff737554de60d4912
Detrav <witalyezep@gmail.com>
2015-05-11 16:03:57 +0300

    Переход с Pcap'a на WinPKFilter

--- Sniffer/Sniffer.cs
+++ Sniffer/Sniffer.cs
@@ -1,8 +1,10 @@
-﻿using SharpPcap;
+﻿using NdisApiWrapper;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,79 +13,155 @@ namespace Sniffer
 {
     public class Sniffer
     {
-        private ICaptureDevice captureDevice { get; set; }
-        private string serverIp;
+        //Драйвер на перехват
+        private IntPtr driverPtr = Ndisapi.OpenFilterDriver();
+        private TCP_AdapterList adapters = new TCP_AdapterList();
+        public bool ready { get; private set; }
+        private Thread threadLookingForPacket;
+        private INTERMEDIATE_BUFFER buffer;
+        private IntPtr bufferPtr;
+        private ETH_REQUEST request;
+        //Для обработки
+        private string server;
         private static byte[] initPacket = new byte[4] { 0x01, 0x00, 0x00, 0x00 };
         private Dictionary<string, Client> clients;
         public delegate void OnParsePacket(string port,string ip, Packet packet);
         public event OnParsePacket onParsePacket;
-        Thread thread; bool needToStop = false;
+        private Thread threadParsePacket;
+        bool needToStop = false;
 
         public Sniffer(string device, string serverIp)
         {
-            CaptureDeviceList deviceList = CaptureDeviceList.Instance;
-            captureDevice = null;
-            foreach(var d in deviceList)
+            GCHandle.Alloc(adapters);
+            if ((Ndisapi.IsDriverLoaded(driverPtr)))
             {
-                if (d.Description == device)
-                {
-                    captureDevice = d;
-                    break;
-                }
+                ready = true;
             }
-            if (captureDevice == null) throw new NullReferenceException("Не существует такого устройства!");
-            captureDevice.OnPacketArrival += captureDevice_OnPacketArrival;
-            captureDevice.Open(DeviceMode.Promiscuous, 0);
-            this.serverIp = serverIp;
+            this.server = serverIp;
             clients = new Dictionary<string, Client>();
-            thread = new Thread(parsePacket);
+            threadLookingForPacket = new Thread(lookingForPacket);
+            threadParsePacket = new Thread(parsePacket);
+
         }
 
-        static public string[] getDevices()
+        public string[] getDevices()
         {
-            CaptureDeviceList deviceList = CaptureDeviceList.Instance;
-            List<string> devices = new List<string>();
-            foreach(var d in deviceList)
+            bool flag = Ndisapi.GetTcpipBoundAdaptersInfo(driverPtr, ref adapters);
+            if (!flag) return null;
+            string[] result = new string[adapters.m_nAdapterCount];
+            for (int i = 0; i < adapters.m_nAdapterCount; i++)
             {
-                devices.Add(d.Description);
+                result[i] = adapters.GetName(i);
             }
-            return devices.ToArray();
+            return result;
         }
 
-        public void start()
+        public void start(int num)
         {
-            captureDevice.Filter += "host " + serverIp;
-            thread.Start();
-            captureDevice.Capture();
+            if (ready)
+            {
+                ADAPTER_MODE mode = new ADAPTER_MODE
+                {
+                    dwFlags = Ndisapi.MSTCP_FLAG_SENT_LISTEN | Ndisapi.MSTCP_FLAG_RECV_LISTEN,
+                    hAdapterHandle = adapters.m_nAdapterHandle[num]
+                };
+                Ndisapi.SetAdapterMode(driverPtr, ref mode);
+                IP_ADDRESS_V4 serverIp = new IP_ADDRESS_V4
+                {
+                    m_AddressType = Ndisapi.IP_SUBNET_V4_TYPE,
+                    m_IpSubnet = new IP_SUBNET_V4
+                    {
+                        m_Ip = BitConverter.ToUInt32(IPAddress.Parse(server).GetAddressBytes(), 0),
+                        m_IpMask = 0xFFFFFFFF
+                    }
+                };
+                //Filters
+                STATIC_FILTER_TABLE filtersTable = new STATIC_FILTER_TABLE();
+                filtersTable.m_StaticFilters = new STATIC_FILTER[3];
+
+                filtersTable.m_TableSize = 3;
+                filtersTable.m_StaticFilters[0].m_Adapter = 0; // applied to all adapters
+                filtersTable.m_StaticFilters[0].m_ValidFields = Ndisapi.NETWORK_LAYER_VALID;
+                filtersTable.m_StaticFilters[0].m_FilterAction = Ndisapi.FILTER_PACKET_REDIRECT;
+                filtersTable.m_StaticFilters[0].m_dwDirectionFlags = Ndisapi.PACKET_FLAG_ON_SEND;
+                filtersTable.m_StaticFilters[0].m_NetworkFilter.m_dwUnionSelector = Ndisapi.IPV4;
+                filtersTable.m_StaticFilters[0].m_NetworkFilter.m_IPv4.m_ValidFields = Ndisapi.IP_V4_FILTER_DEST_ADDRESS;
+                filtersTable.m_StaticFilters[0].m_NetworkFilter.m_IPv4.m_DestAddress = serverIp;
+                filtersTable.m_StaticFilters[1].m_Adapter = 0; // applied to all adapters
+                filtersTable.m_StaticFilters[1].m_ValidFields = Ndisapi.NETWORK_LAYER_VALID;
+                filtersTable.m_StaticFilters[1].m_FilterAction = Ndisapi.FILTER_PACKET_REDIRECT;
+                filtersTable.m_StaticFilters[1].m_dwDirectionFlags = Ndisapi.PACKET_FLAG_ON_RECEIVE;
+                filtersTable.m_StaticFilters[1].m_NetworkFilter.m_dwUnionSelector = Ndisapi.IPV4;
+                filtersTable.m_StaticFilters[1].m_NetworkFilter.m_IPv4.m_ValidFields = Ndisapi.IP_V4_FILTER_SRC_ADDRESS;
+                filtersTable.m_StaticFilters[1].m_NetworkFilter.m_IPv4.m_SrcAddress = serverIp;
+                filtersTable.m_StaticFilters[2].m_Adapter = 0; // applied to all adapters
+                filtersTable.m_StaticFilters[2].m_ValidFields = 0;
+                filtersTable.m_StaticFilters[2].m_FilterAction = Ndisapi.FILTER_PACKET_PASS;
+                filtersTable.m_StaticFilters[2].m_dwDirectionFlags = Ndisapi.PACKET_FLAG_ON_RECEIVE | Ndisapi.PACKET_FLAG_ON_SEND;
+                Ndisapi.SetPacketFilterTable(driverPtr, ref filtersTable);
+
+                // Allocate and initialize packet structures
+                buffer = new INTERMEDIATE_BUFFER();
+                bufferPtr = Marshal.AllocHGlobal(Marshal.SizeOf(buffer));
+                Win32Api.ZeroMemory(bufferPtr, Marshal.SizeOf(buffer));
+
+                request = new ETH_REQUEST
+                {
+                    hAdapterHandle = adapters.m_nAdapterHandle[num],
+                    EthPacket = { Buffer = bufferPtr }
+                };
+
+                threadLookingForPacket.Start();
+                threadParsePacket.Start();
+                ready = false;
+            }
         }
 
         public void stop()
         {
-            captureDevice.StopCapture();
-            needToStop = true;
+                needToStop = true;
         }
 
-         ~Sniffer()
+        ~Sniffer()
         {
             stop();
         }
 
-        void captureDevice_OnPacketArrival(object sender, CaptureEventArgs e)
+        void lookingForPacket()
         {
-            PacketDotNet.Packet packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+            while(true)
+            {
+                if(needToStop)
+                {
+                    Ndisapi.CloseFilterDriver(driverPtr);
+                    return;
+                }
+                if (Ndisapi.ReadPacket(driverPtr, ref request))
+                {
+                    buffer = (INTERMEDIATE_BUFFER)Marshal.PtrToStructure(bufferPtr, typeof(INTERMEDIATE_BUFFER));
+                    captureDevice_OnPacketArrival(buffer);
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(16);
+                }
+            }
+        }
+
+        void captureDevice_OnPacketArrival(INTERMEDIATE_BUFFER packetBuffer)
+        {
+            PacketDotNet.Packet packet = PacketDotNet.Packet.ParsePacket(PacketDotNet.LinkLayers.Ethernet, packetBuffer.m_IBuffer);
             PacketDotNet.TcpPacket tcpPacket = (PacketDotNet.TcpPacket)packet.Extract(typeof(PacketDotNet.TcpPacket));
             PacketDotNet.IpPacket ipPacket = (PacketDotNet.IpPacket)packet.Extract(typeof(PacketDotNet.IpPacket));
             if (tcpPacket != null && ipPacket != null)
             {
-                DateTime time = e.Packet.Timeval.Date;
-                int len = e.Packet.Data.Length;
                 var srcIp = ipPacket.SourceAddress.ToString();
                 var dstIp = ipPacket.DestinationAddress.ToString();
                 var srcPort = tcpPacket.SourcePort.ToString();
                 var dstPort = tcpPacket.DestinationPort.ToString();
                 var data = tcpPacket.PayloadData;
 
-                if(srcIp == serverIp) // Клиент <- Сервер
+                if(srcIp == server) // Клиент <- Сервер
                 {
                     Client c;
                     if (StructuralComparisons.StructuralEqualityComparer.Equals(initPacket, data))
@@ -94,7 +172,7 @@ namespace Sniffer
                         }
                         else
                         {
-                            c = new Client(dstPort, serverIp);
+                            c = new Client(dstPort, server);
                             clients.Add(dstPort, c);
                         }
                     }
@@ -104,7 +182,7 @@ namespace Sniffer
                     }
 
                 }
-                else if(dstIp == serverIp) // Клиент -> Сервер
+                else if(dstIp == server) // Клиент -> Сервер
                 {
                     Client c;
                     if (clients.TryGetValue(srcPort, out c))
@@ -112,7 +190,6 @@ namespace Sniffer
                         c.send((byte[])data.Clone());  
                     }
                 }
-
             }
         }
 
