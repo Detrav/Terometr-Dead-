81f7c2bad97d60fc6a2e1ac4904d18943d52669a
Detrav <witalyezep@gmail.com>
2015-05-12 14:20:15 +0300

    Fix TCP клиента

--- Sniffer/TCPClient.cs
+++ Sniffer/TCPClient.cs
@@ -7,15 +7,14 @@ namespace Sniffer
 {
     internal class TcpClient
     {
-        private Connection connection1;
-
-        public TcpClient(Connection connection1)
-        {
-            // TODO: Complete member initialization
-            this.connection1 = connection1;
-        }
         public Client teraClient {get; private set;}
         public Connection connection { get; private set; }
+
+        public TcpClient(Connection connection)
+        {
+            // TODO: Complete member initialization
+            this.connection = connection;
+        }
 
         internal void reConstruction(PacketDotNet.TcpPacket tcpPacket)
         {
