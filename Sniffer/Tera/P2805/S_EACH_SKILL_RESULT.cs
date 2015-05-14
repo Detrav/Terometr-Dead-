4d3e218e46c2f3a6ea7b45a58320dc7ffb30eeba
Detrav <witalyezep@gmail.com>
2015-05-14 16:25:39 +0300

    Описание S_EACH_SKILL_RESULT

--- Sniffer/Tera/P2805/S_EACH_SKILL_RESULT.cs
+++ Sniffer/Tera/P2805/S_EACH_SKILL_RESULT.cs
@@ -12,9 +12,20 @@ namespace Sniffer.Tera.P2805
         {
             try
             {
-                readUInt32("timesteps");
-                readUInt64("attacker");
-                readUInt64("attacked");
+                readUInt32("visualeffect+count");//4 
+                readUInt64("attacker id");//8
+                readUInt64("target id");//16
+                readUInt32("creature model id");//24
+                readUInt32("skill id");//28
+                readUInt32("shift 1");//32
+                readUInt64("attak id");//36
+                readUInt32("shift 2");//44
+                readUInt32("damage");//48
+                readUInt16("type");//52
+                readByte("crit");//54
+                readByte("електровсплеск");//55
+                readByte("overturned 1");//56
+                readByte("overturned 2");//57
             }
             catch { }
         }
