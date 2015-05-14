c42d9ae72202ecd4edc04b5552a75bc9fcbc92fa
Detrav <witalyezep@gmail.com>
2015-05-15 01:53:09 +0300

    Simple WPF

--- Terometr/MainWindow.xaml.cs
+++ Terometr/MainWindow.xaml.cs
@@ -24,5 +24,40 @@ namespace Terometr
         {
             InitializeComponent();
         }
+
+        Sniffer.Capture sniffer;
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            sniffer = new Sniffer.Capture();
+            sniffer.getDevices();
+            sniffer.serverIp = "91.225.237.8";
+            sniffer.onParsePacket += sniffer_onParsePacket;
+            sniffer.start(0);
+            dataGrid.Items.Add(new string[3] { i.ToString(), "player name", "player id" });
+            i++;
+        }
+        int i = 0;
+        void sniffer_onParsePacket(Sniffer.Connection connection, Sniffer.Tera.TeraPacket packet)
+        {
+            if(packet.opCode == (ushort)Sniffer.Tera.OpCode2805.S_SPAWN_USER)
+            {
+                var p =Sniffer.Tera.TeraPacketCreator.create(packet);
+                dataGrid.Dispatcher.Invoke(new Action<Sniffer.Tera.TeraPacketParser>((pac) =>
+                {
+                    dataGrid.Items.Add(new string[3] { i.ToString(), pac["name"].value.ToString(), pac["player id"].value.ToString() });
+                }), p);
+                i++;
+            }
+        }
+
+        private void dataGrid_Unloaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            sniffer.stop();
+        }
     }
 }
