b6c6548d76bb032f60ac8a340678baec3b0bce48
Detrav <witalyezep@gmail.com>
2015-05-16 00:40:17 +0300

    Revert "Пакеты через отрибуты"
    This reverts commit de61dfa473671afedf40bc11fd07412cb9f88900.

--- Sniffer/Tera/TeraPacket.cs
+++ Sniffer/Tera/TeraPacket.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,173 +9,17 @@ namespace Sniffer.Tera
     public class TeraPacket
     {
         public Type type;
-        [PacketAttribute(0)]
-        public ushort size { get; set; }
-        [PacketAttribute(2)]
-        public ushort opCode { get; set; }
+        public ushort size;
+        public ushort opCode;
         public byte[] data;
 
         public TeraPacket(byte[] _data, Type _type)
         {
             data = (byte[])_data.Clone();
+            size = BitConverter.ToUInt16(data, 0);//Размер
+            opCode = BitConverter.ToUInt16(data, 2);//id пакета или OpCode
             type = _type;
-
-            foreach (PropertyInfo property in this.GetType().GetProperties())
-            {
-                var attr = (PacketAttribute)property.GetCustomAttribute(typeof(PacketAttribute));
-                if (attr != null)
-                {
-                    switch (property.PropertyType.ToString())
-                    {
-                        case "BitArray":
-                            property.SetValue(this, new System.Collections.BitArray(new byte[1] { data[attr.start] }));
-                            break;
-                        case "byte":
-                            property.SetValue(this, data[attr.start]);
-                            break;
-                        case "sbyte":
-                            property.SetValue(this, (sbyte)data[attr.start]);
-                            break;
-                        case "System.UInt16":
-                            property.SetValue(this, BitConverter.ToUInt16(data, attr.start));
-                            break;
-                        case "short":
-                            property.SetValue(this, BitConverter.ToInt16(data, attr.start));
-                            break;
-                        case "uint":
-                            property.SetValue(this, BitConverter.ToUInt32(data, attr.start));
-                            break;
-                        case "int":
-                            property.SetValue(this, BitConverter.ToInt32(data, attr.start));
-                            break;
-                        case "ulong":
-                            property.SetValue(this, BitConverter.ToUInt64(data, attr.start));
-                            break;
-                        case "long":
-                            property.SetValue(this, BitConverter.ToInt64(data, attr.start));
-                            break;
-                        case "float":
-                            property.SetValue(this, BitConverter.ToSingle(data, attr.start));
-                            break;
-                        case "double":
-                            property.SetValue(this, BitConverter.ToDouble(data, attr.start));
-                            break;
-                        case "char":
-                            property.SetValue(this, Convert.ToChar(BitConverter.ToUInt16(data, attr.start)));
-                            break;
-                        case "string":
-                            property.SetValue(this, byteArrayToString(data, attr.start, attr.start+attr.size));
-                            break;
-                        case "boolean"://Нужно исправить :)
-                            property.SetValue(this, BitConverter.ToBoolean(data, attr.start));
-                            break;
-                        case "byte[]":
-                            property.SetValue(this, copyByteArray(data,attr.start,attr.size));
-                            break;
-                    }
-                }
-            }
         }
-
-        public TeraPacket(TeraPacket packet) : this(packet.data, packet.type) { }
-
-        public override string ToString()
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.Append("Offset 00 01 02 03 04 05 06 07 | 08 09 0A 0B 0C 0D 0E 0F  0123456789ABCDEF\n");
-            for (int i = 0; i < data.Length; i += 16)
-            {
-                sb.AppendFormat(" {0:X4}: {1,-24}| {2,-24} {3,-16}\n", i, byteArrayToHexString(data, i, 8), byteArrayToHexString(data, i + 8, 8), byteArrayToCharArray(data, i, 16));
-            }
-            sb.Append("\n");
-            foreach (PropertyInfo property in this.GetType().GetProperties())
-            {
-                var attr = (PacketAttribute)property.GetCustomAttribute(typeof(PacketAttribute));
-                if (attr != null)
-                {
-                    string str = "{0:X4} - {1} : {2} : {3}\n";
-                    string str_with_shift = "{0:X4}+{1} - {2} : {3} : {4}\n";
-                    switch (property.PropertyType.ToString())
-                    {
-                        case "boolean":
-                            sb.AppendFormat(str_with_shift, attr.start, attr.shift, property.Name, property.GetValue(this), property.PropertyType.ToString());
-                            break;
-                        case "bitarray":
-                            sb.AppendFormat(str, attr.start, property.Name, bitArrayToString(property.GetValue(this) as System.Collections.BitArray), property.PropertyType.ToString());
-                            break;
-                        default:
-                            sb.AppendFormat(str, attr.start, property.Name, property.GetValue(this), property.PropertyType.ToString());
-                            break;
-                    }
-                }
-            }
-            return sb.ToString();
-        }
-
-        public static byte[] copyByteArray(byte[] data, ushort p1, ushort len)
-        {
-            byte[] b = new byte[len];
-            for (int i = 0; i < len; i++)
-                b[i] = data[p1 + i];
-            return b;
-        }
-
-        public static string byteArrayToString(byte[] data, int start, int len = int.MaxValue)
-        {
-            StringBuilder result = new StringBuilder();
-            for (int i = start; i < data.Length && i < len; i += 2)
-            {
-                char c = Convert.ToChar(BitConverter.ToUInt16(data,start));
-                if (c == '\0')
-                    return result.ToString();
-                else
-                    result.Append(c);
-            }
-            return result.ToString();
-        }
-
-        public static string byteArrayToHexString(byte[] data, int start, int length)
-        {
-            StringBuilder result = new StringBuilder();
-            for (int i = start; i < data.Length && i < start + length; i++)
-                result.AppendFormat("{0:X2} ", data[i]);
-            return result.ToString();
-        }
-        public static string byteArrayToHexStringRightToLeft(byte[] data, int start, int length)
-        {
-            StringBuilder result = new StringBuilder();
-            for (int i = start; i < data.Length && i < start + length; i++)
-                result.AppendFormat("{0:X2}", data[i]);
-            return result.ToString();
-        }
-
-        private static string byteArrayToCharArray(byte[] data, int start, int length)
-        {
-            StringBuilder result = new StringBuilder();
-            for (int i = start; i < data.Length && i < start + length; i++)
-            {
-                char c = Convert.ToChar(data[i]);
-                if (char.IsControl(c))
-                {
-                    result.Append('.');
-                }
-                else
-                    result.Append(c);
-            }
-            return result.ToString();
-        }
-
-        private static string bitArrayToString(System.Collections.BitArray bits)
-        {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < bits.Count; i++)
-                if (bits[i])
-                    sb.Append(1);
-                else
-                    sb.Append(0);
-            return sb.ToString();
-        }
-
         public enum Type { Send, Recv }
 
         /*
---
+++ Sniffer/Tera/TeraPacketParser.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sniffer.Tera
+{
+    public class TeraPacketParser : TeraPacket
+    {
+
+        //MemoryStream ms;
+        //BinaryReader br;
+        List<PacketElement> elements;
+
+
+        public TeraPacketParser(TeraPacket packet)
+            : base(packet.data, packet.type)
+        {
+            //ms = new MemoryStream(packet.data);
+            //br = new BinaryReader(ms);
+            elements = new List<PacketElement>();
+            readUInt16(0,"size");
+            readUInt16(2,"opcode");
+        }
+
+        public PacketElement this[string str]
+        {
+            get
+            {
+                foreach (var el in elements)
+                    if (el.name == str)
+                        return el;
+                return null;
+            }
+            set
+            {
+                foreach (var el in elements)
+                    if (el.name == str)
+                    {
+                        el.shift = value.shift;
+                        el.start = value.start;
+                        el.type = value.type;
+                        el.value = value.value;
+                        return;
+                    }
+                elements.Add(value);
+            }
+        }
+
+        protected System.Collections.BitArray readBitArray(ushort pos,string p)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                value = new System.Collections.BitArray(new byte[1] { data[pos] }),
+                type = "bitarray"
+            };
+            this[p] = el;
+            return (System.Collections.BitArray)el.value;
+        }
+        protected byte readByte(ushort pos,string p)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                value = data[pos],
+                type = "byte"
+            }; 
+            this[p] =el;
+            return (byte)el.value;
+        }
+        protected sbyte readSByte(ushort pos,string p)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                value = (sbyte)data[pos],
+                type = "sbyte"
+            };
+            this[p] = el;
+            return (sbyte)el.value;
+        }
+        protected ushort readUInt16(ushort pos,string p)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                value = BitConverter.ToUInt16(data,pos),
+                type = "ushort"
+            };
+            this[p] = el;
+            return (ushort)el.value;
+        }
+        protected short readInt16(ushort pos,string p)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                value = BitConverter.ToInt16(data, pos),
+                type = "short"
+            };
+            this[p] = el;
+            return (short)el.value;
+        }
+        protected uint readUInt32(ushort pos, string p)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                value = BitConverter.ToUInt32(data,pos),
+                type = "uint"
+            };
+            this[p] = el;
+            return (uint)el.value;
+        }
+        protected int readInt32(ushort pos ,string p)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                value = BitConverter.ToInt32(data,pos),
+                type = "int"
+            };
+            this[p] = el;
+            return (int)el.value;
+        }
+        protected ulong readUInt64(ushort pos,string p)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                value = BitConverter.ToUInt64(data,pos),
+                type = "ulong"
+            };
+            this[p] = el;
+            return (ulong)el.value;
+        }
+        protected long readInt64(ushort pos,string p)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                value = BitConverter.ToInt64(data,pos),
+                type = "long"
+            };
+            this[p] = el;
+            return (long)el.value;
+        }
+        protected float readSingle(ushort pos,string p)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                value = BitConverter.ToSingle(data,pos),
+                type = "float"
+            };
+            this[p] = el;
+            return (float)el.value;
+        }
+        protected double readDouble(ushort pos, string p)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                value = BitConverter.ToDouble(data,pos),
+                type = "double"
+            };
+            this[p] = el;
+            return (double)el.value;
+        }
+        protected char readChar(ushort pos, string p)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                value = BitConverter.ToChar(data,pos),
+                type = "char"
+            };
+            this[p] = el;
+            return (char)el.value;
+        }
+        protected string readString(ushort pos,string p, int len = int.MaxValue)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                type = "string"
+            };
+            ushort start = pos;
+            StringBuilder result = new StringBuilder();
+            for (int i = start; i<len ; i += 2)
+            {
+                char c = Convert.ToChar(BitConverter.ToUInt16(data,i));
+                if (c == '\0')
+                    break;
+                else
+                    result.Append(c);
+            }
+            el.value = result.ToString();
+            this[p] = el;
+            return (string)el.value;
+        }
+
+        protected bool readBoolean(ushort pos,string p, byte s = 0)
+        {
+            if (s > 7)
+                return false;
+            var temp = new System.Collections.BitArray(new byte[1] { data[pos] });
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                value = temp[s],
+                shift = s,
+                type = "boolean"
+            };
+            this[p] = el;
+            return (bool)el.value;
+        }
+
+        protected string readHex(ushort pos,string p, int lenght)
+        {
+            PacketElement el = new PacketElement
+            {
+                name = p,
+                start = pos,
+                type = "hex"
+            };
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i< lenght; i++)
+                result.AppendFormat("{0:X2}", data[i]);
+            el.value = result.ToString();
+            this[p] = el;
+            return (string)el.value;
+        }
+
+        public class PacketElement
+        {
+            public string name = null;
+            public ushort start = 0;//в байтах
+            public byte shift = 0;//в битах
+            public object value = null;
+            public string type = null;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Offset 00 01 02 03 04 05 06 07 | 08 09 0A 0B 0C 0D 0E 0F  0123456789ABCDEF\n");
+            for (int i = 0; i < data.Length; i += 16)
+            {
+                sb.AppendFormat(" {0:X4}: {1,-24}| {2,-24} {3,-16}\n", i, byteArrayToHexString(data, i, 8), byteArrayToHexString(data, i + 8, 8), byteArrayToCharArray(data, i, 16));
+            }
+            sb.Append("\n");
+            foreach (var el in elements)
+            {
+                string str = "{0:X4} - {1} : {2} : {3}\n";
+                string str_with_shift = "{0:X4}+{1} - {2} : {3} : {4}\n";
+                switch (el.type)
+                {
+                    case "boolean":
+                        sb.AppendFormat(str_with_shift, el.start, el.shift, el.name, el.value, el.type);
+                        break;
+                    case "bitarray":
+                        sb.AppendFormat(str, el.start, el.name, bitArrayToString(el.value as System.Collections.BitArray), el.type);
+                        break;
+                    default:
+                        sb.AppendFormat(str, el.start, el.name, el.value, el.type);
+                        break;
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static string byteArrayToHexString(byte[] data, int start, int length)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = start; i < data.Length && i < start + length; i++)
+                result.AppendFormat("{0:X2} ", data[i]);
+            return result.ToString();
+        }
+        public static string byteArrayToHexStringRightToLeft(byte[] data, int start, int length)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = start; i < data.Length && i < start + length; i++)
+                result.AppendFormat("{0:X2}", data[i]);
+            return result.ToString();
+        }
+        private static string byteArrayToCharArray(byte[] data, int start, int length)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = start; i < data.Length && i < start + length; i++)
+            {
+                char c = Convert.ToChar(data[i]);
+                if (char.IsControl(c))
+                {
+                    result.Append('.');
+                }
+                else
+                    result.Append(c);
+            }
+            return result.ToString();
+        }
+        public static string byteArrayToString(byte[] data, int start,int len = int.MaxValue)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = start; i < data.Length && i<len; i+=2)
+            {
+                char c = BitConverter.ToChar(data,i);
+                if (c == '\0')
+                    return result.ToString();
+                else
+                    result.Append(c);
+            }
+            return result.ToString();
+        }
+
+        private static string bitArrayToString(System.Collections.BitArray bits)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bits.Count ; i++)
+                if (bits[i])
+                    sb.Append(1);
+                else
+                    sb.Append(0);
+            return sb.ToString();
+        }
+    }
+}
