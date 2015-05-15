ad32a63c3e5ee9169e961fa419ad78c8b7156d40
Detrav <witalyezep@gmail.com>
2015-05-15 21:25:45 +0300

    80% готовое ГУИ для ДПС метра

---
+++ Terometr/Themes/DpsRow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Terometr.Themes
+{
+    /// <summary>
+    /// Логика взаимодействия для DpsRow.xaml
+    /// </summary>
+    public partial class DpsRow : UserControl
+    {
+        public DpsRow()
+        {
+            InitializeComponent();
+        }
+
+        public DpsRow(double d, string s1, string s2): this()
+        {
+            procent = d;
+            playerName = s1;
+            playerCount = s2;
+        }
+
+        public double procent { get; set; }
+
+        /*public static readonly DependencyProperty procentProperty =
+            DependencyProperty.Register("procent",typeof(double),typeof(DpsRow),0.0);*/
+
+        public string playerName { get; set; }
+
+        public string playerCount { get; set; }
+    }
+}
--- Terometr/Windows/DpsWindow.xaml.cs
+++ Terometr/Windows/DpsWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Terometr.Themes;
 
 namespace Terometr.Windows
 {
@@ -22,17 +23,47 @@ namespace Terometr.Windows
         public DpsWindow()
         {
             InitializeComponent();
-            
+            myNotifyIcon.Icon = System.Drawing.Icon.FromHandle(Properties.Resources.icon.GetHicon());
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
 
         private void buttonClose_Click(object sender, RoutedEventArgs e)
         {
+            Hide();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Add(new DpsRow( 100.0, "Detrav", "1450{40%}" ));
+            listBox.Items.Add(new DpsRow( 78.0, "Pofigist", "250{27%}" ));
+            listBox.Items.Add(new DpsRow( 40.0, "Varted", "145{10%}" ));
+            listBox.Items.Add(new DpsRow(20.0, "Witaly", "14{2%}"));
+            listBox.Items.Add(new DpsRow(100.0, "Detrav", "1450{40%}"));
+            listBox.Items.Add(new DpsRow(78.0, "Pofigist", "250{27%}"));
+            listBox.Items.Add(new DpsRow(40.0, "Varted", "145{10%}"));
+            listBox.Items.Add(new DpsRow(20.0, "Witaly", "14{2%}"));
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            if(this.WindowState == WindowState.Minimized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
             Close();
+        }
+        private void MenuItemShow_Click(object sender, RoutedEventArgs e)
+        {
+            Show();
         }
     }
 }
