4a80a9b965363a4884fea8af06f8bba52fa8fe18
Detrav <witalyezep@gmail.com>
2015-05-16 12:48:47 +0300

    Add структура данных, может понадобится

--- Terometr/Data/PlayerInfo.cs
+++ Terometr/Data/PlayerInfo.cs
@@ -8,8 +8,25 @@ namespace Terometr.Data
 {
     class PlayerInfo
     {
+        /*
+         * Нужны след переменный:
+         * 1) Имя
+         * 2) Номер
+         * 3) В пати(рейде)
+         * 4) Урон в скунду
+         * 4.1) Количество урона? :)
+         * 5) Шанс крита
+         * 6) Последняя активность чтобы считать таймаут метра
+         * 7) Нужно удалить?
+         * Не знаю
+         */
         string name;
         ulong id;
         bool inParty;
+        double dps;
+        ulong damage;
+        double crit;
+        DateTime last;
+        bool delete;
     }
 }
