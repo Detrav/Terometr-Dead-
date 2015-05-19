b95c1afb8219218e0add2a483c5ee6c240b4848c
Detrav <witalyezep@gmail.com>
2015-05-19 17:37:21 +0300

    Add 2 кнопочки + таймер

--- Teroniffer/UserElements/SnifferPage.xaml.cs
+++ Teroniffer/UserElements/SnifferPage.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using Teroniffer.Core;
 
 namespace Teroniffer.UserElements
@@ -23,13 +24,39 @@ namespace Teroniffer.UserElements
     public partial class SnifferPage : UserControl
     {
         List<DataPacket> packets = new List<DataPacket>();
-
+        DispatcherTimer timerStatusBar;
         /*internal IEnumerable dataPackets { get { return (IEnumerable)GetValue(dataPacketsProperty); } set { SetValue(dataPacketsProperty, value); } }
         public static readonly DependencyProperty dataPacketsProperty = DependencyProperty.Register("dataPackets", typeof(IEnumerable), typeof(SnifferPage), null);*/
         
         public SnifferPage()
         {
             InitializeComponent();
+            timerStatusBar = new DispatcherTimer();
+            timerStatusBar.Interval = TimeSpan.FromMilliseconds(101);
+            timerStatusBar.Tick += timerStatusBar_Tick;
+            timerStatusBar.Start();
+        }
+
+        void timerStatusBar_Tick(object sender, EventArgs e)
+        {
+            timerStatusBar.Stop();
+            int count = 0;
+            lock(packets)
+            {
+                count = packets.Count;
+            }
+            labelPacketCount.Content = count.ToString();
+            double m = GC.GetTotalMemory(true);
+            int order = 0;
+            while (m >= 1024 && order + 1 < sizes.Length)
+            {
+                order++;
+                m = m / 1024;
+            }
+
+            labelMemoryUsage.Content = String.Format("{0:0.##} {1}", m, sizes[order]);
+            timerStatusBar.Start();
+
         }
 
         private void buttonNew_Click(object sender, RoutedEventArgs e)
@@ -65,6 +92,12 @@ namespace Teroniffer.UserElements
         private void buttonPrev_Click(object sender, RoutedEventArgs e)
         {
 
+            int take = 1000;
+            Int32.TryParse(textBoxCount.Text, out take);
+            int skip = 0;
+            Int32.TryParse(textBoxSkip.Text, out skip);
+            textBoxSkip.Text = (skip - take).ToString();
+            buttonRefresh_Click(this, e);
         }
 
         private void buttonRefresh_Click(object sender, RoutedEventArgs e)
@@ -83,7 +116,12 @@ namespace Teroniffer.UserElements
 
         private void buttonNext_Click(object sender, RoutedEventArgs e)
         {
-
+            int take = 1000;
+            Int32.TryParse(textBoxCount.Text, out take);
+            int skip = 0;
+            Int32.TryParse(textBoxSkip.Text, out skip);
+            textBoxSkip.Text = (skip + take).ToString();
+            buttonRefresh_Click(this, e);
         }
 
         private void buttonFilterImport_Click(object sender, RoutedEventArgs e)
@@ -96,11 +134,12 @@ namespace Teroniffer.UserElements
 
         }
 
+        static string[] sizes = { "B", "KB", "MB", "GB" };
         internal void parsePacket(Detrav.Sniffer.Tera.TeraPacket teraPacket)
         {
-            lock(packets)
+            lock (packets)
             {
-                packets.Add(new DataPacket(packets.Count,teraPacket));
+                packets.Add(new DataPacket(packets.Count, teraPacket));
             }
         }
     }
