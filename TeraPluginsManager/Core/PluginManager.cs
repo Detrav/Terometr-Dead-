e024cf1b956b2875907c57c2642f13bc2ce3ab7a
Detrav <witalyezep@gmail.com>
2015-05-21 22:01:21 +0300

    Промежуточный вариант

---
+++ TeraPluginsManager/Core/PluginManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detrav.TeraPluginsManager.Core
+{
+    class PluginManager
+    {
+        Assembly[] assemblies;
+        public PluginManager()
+        {
+            var files = Directory.GetFiles("plugins", "*.dll");
+            List<Assembly> l = new List<Assembly>();
+            foreach(var file in files)
+            {
+                l.Add(Assembly.LoadFile(file));
+            }
+            assemblies = l.ToArray();
+        }
+
+        public Type[] getTypes()
+        {
+            Type[] result = new Type[assemblies.Length];
+        }
+    }
+}
---
+++ TeraPluginsManager/Core/TeraConnection.cs
@@ -0,0 +1,43 @@
+﻿using Detrav.TeraApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detrav.TeraPluginsManager.Core
+{
+    class TeraConnection : ITeraConnection
+    {
+        IPlugin[] plugins;
+        public TeraConnection(Type[] types)
+        {
+            plugins = new IPlugin[types.Length];
+            for (int i = 0; i < types.Length; i++)
+            {
+                plugins[i] = Activator.CreateInstance(t) as IPlugin;
+            }
+        }
+
+        public event OnLogin onLogin;
+
+        public event OnTick onTick;
+
+        public void doEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void unRegister()
+        {
+            foreach (var p in plugins)
+                p.unRegister();
+        }
+
+        public void register()
+        {
+            foreach (var p in plugins)
+                p.register(this);
+        }
+    }
+}
