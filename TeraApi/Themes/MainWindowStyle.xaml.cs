34e52cdfcd9f004738c0242eddabe1541b671227
Detrav <witalyezep@gmail.com>
2015-05-21 18:41:48 +0300

    And now rlyCleared

---
+++ TeraApi/Themes/MainWindowStyle.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Detrav.TeraApi.Themes
+{
+    internal static class LocalExtensions
+    {
+        public static void ForWindowFromTemplate(this object templateFrameworkElement, Action<Window> action)
+        {
+            Window window = ((FrameworkElement)templateFrameworkElement).TemplatedParent as Window;
+            if (window != null) action(window);
+        }
+
+        public static IntPtr GetWindowHandle(this Window window)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(window);
+            return helper.Handle;
+        }
+    }
+    /// <summary>
+    /// Логика взаимодействия для MainWindowStyle.xaml
+    /// </summary>
+    public partial class MainWindowStyle
+    {
+
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            sender.ForWindowFromTemplate(w => SystemCommands.MinimizeWindow(w));
+        }
+
+        private void Button_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            sender.ForWindowFromTemplate(w => SystemCommands.CloseWindow(w));
+        }
+    }
+}
