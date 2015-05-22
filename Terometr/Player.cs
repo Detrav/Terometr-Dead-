b62467004c255a0cb9235c7f4a1a48f605ac9a52
Detrav <witalyezep@gmail.com>
2015-05-22 17:47:20 +0300

    Add player class to dps

---
+++ Terometr/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detrav.Terometr
+{
+    class Player
+    {
+        ulong id;
+        string name;
+        double damage;
+        double dps { get { return damage / span.TotalSeconds; } }
+        TimeSpan span { get { return start - stop; } }
+        DateTime start = DateTime.MinValue;
+        DateTime stop = DateTime.MinValue;
+
+        public Player(ulong id,string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void startBattle()
+        {
+            start += DateTime.Now - stop;
+        }
+
+        public void stopBattle()
+        {
+            stop = DateTime.Now;
+        }
+
+        public void dmg(double damage)
+        {
+            this.damage += damage;
+        }
+    }
+}
