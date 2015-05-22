6b3a5ad6b68f10ac8f1bef9bf3ac1f6f0022dfdf
Detrav <witalyezep@gmail.com>
2015-05-22 16:26:36 +0300

    Add Terometr Plugin

---
+++ Terometr/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using Detrav.TeraApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terometr
+{
+    class Plugin : IPlugin
+    {
+
+        public static void register()
+        {
+            //System.Windows.MessageBox.Show("Registered!");
+        }
+        MainWindow w;
+        public void load(ITeraConnection parent)
+        {
+            w = new MainWindow();
+            show();
+        }
+
+        public void show()
+        {
+            w.Show();
+            //w.WindowState = System.Windows.WindowState.Normal;
+        }
+
+        public void unLoad()
+        {
+            w.close = true;
+            w.Close();
+        }
+
+
+        public void hide()
+        {
+            w.Hide();
+            //w.WindowState = System.Windows.WindowState.Minimized;
+        }
+    }
+}
