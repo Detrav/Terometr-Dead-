7d569337feb1fc37540cb18267912a9493e8a5ef
Detrav <witalyezep@gmail.com>
2015-05-22 17:36:05 +0300

    Styled terometr

---
+++ Terometr/PlayerBarElement.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Detrav.Terometr
+{
+    /// <summary>
+    /// Логика взаимодействия для PlayerBarElement.xaml
+    /// </summary>
+    public partial class PlayerBarElement : UserControl
+    {
+        public PlayerBarElement()
+        {
+            InitializeComponent();
+            green = (Brush)br.ConvertFrom("#FF10AE00");
+            blue = (Brush)br.ConvertFrom("#FF002057");
+        }
+
+        BrushConverter br = new BrushConverter();
+        Brush green;
+        Brush blue;
+        
+
+        public void changeData(double progressValue,string left,string right, bool me = false)
+        {
+            if (me) progressBar.Foreground = green;
+            else progressBar.Foreground = blue;
+            labelLeft.Content = left;
+            labelRight.Content = right;
+        }
+    }
+}
